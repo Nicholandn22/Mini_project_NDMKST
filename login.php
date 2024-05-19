17bbce4a2371e1ec97f15d1f7767e7c674aa731a
Nicholandn22 <nicholas.dwinata@ti.ukdw.ac.id>
2024-05-19 13:06:19 +0700

    fix login

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class="navigation">
+          <div id="judul">
+            <img
+              src="/drive-download-20240310T064708Z-001/Logo Mytic (White).png"
+              alt=""
+            />
+            <!-- <h1>My.Tic</h1> -->
+          </div>
+          <div id="kanan">
+            <ul>
+              <a href="main.html">Utama</a>
+              <a href="#">List Konser</a>
+              <a href="main.html">Tentang Kami</a>
+            </ul>
+          </div>
+        </div>
+        <!-- <h1 id="WELCOME">WELCOME</h1> -->
+    </header>
+    
+</body>
+</html>
