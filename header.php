a29e023cd0a8142d7f73ebcd8ecab2a67b540999
Nicholandn22 <nicholas.dwinata@ti.ukdw.ac.id>
2024-05-19 13:18:10 +0700

    fix login

--- header.php
+++ header.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <link rel="stylesheet" href="login.css" />
+   <link rel="stylesheet" href="-" />  <!-- nnt - dignati file css kaleann -->
     <script src="https://unpkg.com/feather-icons"></script>
 </head>
 <body>
+    
 <header>
         <div class="navigation">
           <div id="judul">
@@ -16,7 +17,6 @@
               src="drive-download-20240310T064708Z-001/Logo Mytic (White).png"
               alt=""
             />
-            <!-- <h1>My.Tic</h1> -->
           </div>
           <div id="kanan">
             <ul>
@@ -28,33 +28,6 @@
           </div>
         </div>
     </header>
-
-  <main>
-
-</div>
-
-    <div class="containerBox">
-      <form action="">
-      <h2>Log in</h2>
-      <h4>Email</h4>
-      <input type="text" placeholder="Masukkan Email Anda">
-      <h4>Your Password</h4>
-      <input type="password" placeholder="Masukkan Password Anda">
-      <button>
-  <span class="button_top">Button
-  </span>
-</button>
-<h5>By continuing, you agree to the <a href="#">Terms of use</a> and <a href="#">Privacy Policy</a></h5>
-    <div class="opsi2">
-      <h5>Do not have an account ? <a href="#">Create account</a></h5>
-      <h5><a href="#">Forget your password</a></h5>
-    </div>
-    </form>
-
-    </div>
-  </main>
-  <script>
-      feather.replace();
-  </script> 
+    
 </body>
 </html>
